ca9cbd3efbfff305f64229bfd562ee869d563e2e
Tomas Sarquis <tsarquis88@gmail.com>
2020-09-08 16:10:52 -0300

    delete includes

--- esp-idf/main/include/main_task.h
+++ esp-idf/main/include/main_task.h
@@ -1,30 +1,13 @@
-#include <stdio.h>
 #include <iostream>
-#include <cstring>
-#include <vector>
 #include <cmath>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 #include "driver/i2c.h"
 #include "driver/rtc_io.h"
 
 #include <esp_sleep.h>
-#include <esp_wifi.h>
-#include <esp_system.h>
-#include <esp_event.h>
-#include <esp_event_loop.h>
 #include <esp_log.h>
-#include <esp_pm.h>
-
-#include <nvs_flash.h>
-#include <tcpip_adapter.h>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
 
 #include "../lib/MPU6050/MPU6050.h"
 
