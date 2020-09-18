2f14b71d2b0d14fc3a0511ecacb06388a66c0e03
Tomas Sarquis <tsarquis88@gmail.com>
2020-09-18 11:49:05 -0300

    fix transfer stacksize

--- esp-idf/main/include/transfer_task.h
+++ esp-idf/main/include/transfer_task.h
@@ -3,7 +3,7 @@
 #define TRANSFER_TASK_DELAY                 ( 7000                              )
 #define TRANSFER_TASK_DELAY_1S              ( 1000                              )
 #define TRANSFER_TASK_PRIORITY			    ( tskIDLE_PRIORITY + 2              )
-#define TRANSFER_TASK_STACK_SIZE		    ( configMINIMAL_STACK_SIZE + 2024   )
+#define TRANSFER_TASK_STACK_SIZE		    ( configMINIMAL_STACK_SIZE + 4048   )
 #define TRANSFER_TASK_TAG                   ( ( const char * ) "Transfer" )
 
 void start_transfer_task            ( xQueueHandle*, xQueueHandle*	);
