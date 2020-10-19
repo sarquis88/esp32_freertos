84df4c7024ffc0641caf41c6ef924c38773b10ea
Tomas Sarquis <tsarquis88@gmail.com>
2020-10-19 10:50:52 -0300

    update planilla, remove oldfiles

--- practica/tasks/blinker_task.h
+++
@@ -1,8 +0,0 @@
-#include "main_task.h"
-
-#define BLINKER_DELAY_MS				( ( uint16_t ) 100                  )
-#define BLINKER_TASK_PRIORITY			( tskIDLE_PRIORITY + 2              )
-#define BLINKER_TASK_STACK_SIZE		    ( configMINIMAL_STACK_SIZE + 2024   )
-
-void start_blinker_task         ( void      );
-void prvBlinkerTask             ( void *    );
