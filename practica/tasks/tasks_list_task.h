29b870f23fab45b3f0f52c7a7fe359dd38030813
Tomas Sarquis <tsarquis88@gmail.com>
2020-10-22 10:34:24 -0300

    removed files viejos

--- practica/tasks/tasks_list_task.h
+++
@@ -1,9 +0,0 @@
-#include "main_task.h"
-
-#define TASKSLIST_TASK_DELAY_MS 		    ( ( uint16_t ) 5000                 )
-#define TASKSLIST_TASK_PRIORITY			    ( tskIDLE_PRIORITY + 2              )
-#define TASKSLIST_TASK_STACK_SIZE		    ( configMINIMAL_STACK_SIZE + 2048   )
-#define TASKSLIST_LOGGING_TAG               ( ( const char * ) "TasksList"      )
-
-void start_taskslist_task           ( void                      );
-void prvTasksListTask	            ( void *                    );
